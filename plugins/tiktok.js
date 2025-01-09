8a87b38e2cb2349f4bea27f0fbdc91d27ce38617
mrdinesh595 <mrdineshofc@gmail.com>
2025-01-09 21:25:39 +0530

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,217 +1,69 @@
-/*
-
-$$$$$$\            $$\                                               
-$$  __$$\           $$ |                                              
-$$ /  \__|$$\   $$\ $$$$$$$\  $$$$$$$$\  $$$$$$\   $$$$$$\   $$$$$$\  
-\$$$$$$\  $$ |  $$ |$$  __$$\ \____$$  |$$  __$$\ $$  __$$\ $$  __$$\ 
- \____$$\ $$ |  $$ |$$ |  $$ |  $$$$ _/ $$$$$$$$ |$$ |  \__|$$ /  $$ |
-$$\   $$ |$$ |  $$ |$$ |  $$ | $$  _/   $$   ____|$$ |      $$ |  $$ |
-\$$$$$$  |\$$$$$$  |$$$$$$$  |$$$$$$$$\ \$$$$$$$\ $$ |      \$$$$$$  |
- \______/  \______/ \_______/ \________| \_______|\__|       \______/
-
-Project Name : SubZero MD
-Creator      : Darrell Mucheri ( Mr Frank OFC )
-Repo         : https//github.com/mrfrank-ofc/SUBZERO-MD
-Support      : wa.me/18062212660
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const _0x2dbdf0=_0x7ad8;(function(_0x5b611e,_0x42f0e3){const _0x487ba5=_0x7ad8,_0x299a20=_0x5b611e();while(!![]){try{const _0x45a3ad=parseInt(_0x487ba5(0x8b))/0x1+-parseInt(_0x487ba5(0xa7))/0x2*(-parseInt(_0x487ba5(0xa6))/0x3)+parseInt(_0x487ba5(0xa5))/0x4*(-parseInt(_0x487ba5(0xa2))/0x5)+parseInt(_0x487ba5(0x7e))/0x6*(parseInt(_0x487ba5(0xa8))/0x7)+-parseInt(_0x487ba5(0xa1))/0x8*(parseInt(_0x487ba5(0x80))/0x9)+parseInt(_0x487ba5(0xa0))/0xa*(-parseInt(_0x487ba5(0xad))/0xb)+-parseInt(_0x487ba5(0x95))/0xc*(-parseInt(_0x487ba5(0x9d))/0xd);if(_0x45a3ad===_0x42f0e3)break;else _0x299a20['push'](_0x299a20['shift']());}catch(_0x2247fe){_0x299a20['push'](_0x299a20['shift']());}}}(_0x3c88,0x46e61));function hi(){const _0x122b73=_0x7ad8;console[_0x122b73(0xac)](_0x122b73(0x87));}function _0x3c88(){const _0x288706=['username','57492KMvngy','\x0a┇๏\x20*Shares:*\x20','\x0a┇๏\x20*Comments:*\x20','Sorry,\x20I\x20couldn\x27t\x20process\x20this\x20type\x20of\x20TikTok\x20content.','type','sendMessage','Error\x20in\x20TikTok\x20command:','../command','299oWJksi','http','ttdl','1813930igKpKs','88CyDSZb','5VAFXJE','comments','result','572092YWquDP','225603kreEhi','2YJgBle','3698737vngmQJ','Download\x20videos\x20or\x20images\x20from\x20TikTok.','tiktokdl2','shares','log','33gxbGEj','\x20\x20\x20\x20\x0a┇๏\x20*Description:*\x20','node-fetch','An\x20error\x20occurred\x20while\x20processing\x20your\x20request.\x20Please\x20try\x20again\x20later.','favorite','6SbreZj','N/A','97479yKYzAC','no-watermark','error','&apiKey=yanzdev','json','entries','tools','Hello\x20World!','image','tiktokvid2','description','382959JJbajz','\x20\x20\x20\x20\x0a╰━━━━━━━━━━━━──┈⊷\x0a>\x20©\x20ᴘᴏᴡᴇʀᴇᴅ\x20ʙʏ\x20sᴜʙᴢᴇʀᴏ','video','views','\x0a┇๏\x20*Likes:*\x20','startsWith','tt2','tiktok2','╭━━━〔\x20*SUBZERO-MD*\x20〕━━━┈⊷\x0a┃▸╭───────────\x0a┃▸┃๏\x20*TIKTOK\x20DOWNLOADER*\x0a┃▸└───────────···๏\x0a╰────────────────┈⊷\x0a╭━━〔\x20*Post\x20Details*\x20〕━━┈⊷\x0a┇๏\x20*Type:*\x20'];_0x3c88=function(){return _0x288706;};return _0x3c88();}function _0x7ad8(_0x2c637a,_0xf5e587){const _0x3c8816=_0x3c88();return _0x7ad8=function(_0x7ad890,_0xe384dd){_0x7ad890=_0x7ad890-0x7a;let _0x15ef76=_0x3c8816[_0x7ad890];return _0x15ef76;},_0x7ad8(_0x2c637a,_0xf5e587);}hi();const fetch=require(_0x2dbdf0(0x7b)),{cmd}=require(_0x2dbdf0(0x9c));cmd({'pattern':_0x2dbdf0(0x92),'alias':[_0x2dbdf0(0x91),_0x2dbdf0(0xaa),'ttdown2',_0x2dbdf0(0x89),_0x2dbdf0(0x9f)],'desc':_0x2dbdf0(0xa9),'react':'✅','category':_0x2dbdf0(0x86),'filename':__filename},async(_0x572eda,_0x5725ab,_0x46523f,{from:_0x45c4f6,args:_0x550065,reply:_0x38160c})=>{const _0x40a5ef=_0x2dbdf0;try{const _0x17f2c5=_0x550065[0x0];if(!_0x17f2c5||!_0x17f2c5[_0x40a5ef(0x90)](_0x40a5ef(0x9e)))return _0x38160c('Please\x20provide\x20a\x20valid\x20TikTok\x20link.\x0a\x0a*Usage\x20Example:*\x0a\x0a.tt2\x20<TikTok\x20video\x20URL>');const _0x7dacc7=await fetch('https://api.yanzbotz.live/api/downloader/tiktok?url='+encodeURIComponent(_0x17f2c5)+_0x40a5ef(0x83)),_0x15a6cb=await _0x7dacc7[_0x40a5ef(0x84)]();if(!_0x15a6cb||!_0x15a6cb[_0x40a5ef(0xa4)])return _0x38160c('Sorry,\x20I\x20couldn\x27t\x20fetch\x20the\x20TikTok\x20content.\x20Please\x20check\x20the\x20link\x20and\x20try\x20again.');const _0x596701=_0x15a6cb['result'],_0x4e57a1=_0x40a5ef(0x93)+(_0x596701[_0x40a5ef(0x99)]||_0x40a5ef(0x7f))+'\x0a┇๏\x20*Name:*\x20'+(_0x596701['name']||_0x40a5ef(0x7f))+'\x0a┇๏\x20*Username:*\x20'+(_0x596701[_0x40a5ef(0x94)]||'N/A')+'\x0a┇๏\x20*Views:*\x20'+(_0x596701[_0x40a5ef(0x8e)]||0x0)+_0x40a5ef(0x8f)+(_0x596701['likes']||0x0)+_0x40a5ef(0x97)+(_0x596701[_0x40a5ef(0xa3)]||0x0)+'\x0a┇๏\x20*Favorites:*\x20'+(_0x596701[_0x40a5ef(0x7d)]||0x0)+_0x40a5ef(0x96)+(_0x596701[_0x40a5ef(0xab)]||0x0)+_0x40a5ef(0x7a)+(_0x596701[_0x40a5ef(0x8a)]||_0x40a5ef(0x7f))+_0x40a5ef(0x8c);if(_0x596701[_0x40a5ef(0x99)]===_0x40a5ef(0x8d))await _0x572eda[_0x40a5ef(0x9a)](_0x45c4f6,{'video':{'url':_0x596701[_0x40a5ef(0x8d)][_0x40a5ef(0x81)]},'caption':_0x4e57a1},{'quoted':_0x46523f});else{if(_0x596701['type']===_0x40a5ef(0x88))for(const [_0x1ac338,_0x5746de]of(_0x596701[_0x40a5ef(0x88)]||[])[_0x40a5ef(0x85)]()){await _0x572eda[_0x40a5ef(0x9a)](_0x45c4f6,{'image':{'url':_0x5746de},'caption':'*💜\x20Image:*\x20'+(_0x1ac338+0x1)+'\x0a\x0a'+_0x4e57a1},{'quoted':_0x46523f});}else return _0x38160c(_0x40a5ef(0x98));}}catch(_0x1aefb6){console[_0x40a5ef(0x82)](_0x40a5ef(0x9b),_0x1aefb6),_0x38160c(_0x40a5ef(0x7c));}});
+import fetch from 'node-fetch'
+import ffmpeg from "fluent-ffmpeg"
+
+var handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) {
+        throw `*[❗] Example: ${usedPrefix + command
+        } https://www.tiktok.com/@tuanliebert/video/7313159590349212934?is_from_webapp=1&sender_device=pc`;
+    }
+
+    try {
+        await conn.reply ( m.chat, "Tunggu sebentar kak, video sedang di download...", m, );
+
+        const tiktokData = await tiktokdl(args[0]);
+
+        if (!tiktokData) {
+            throw "Gagal mendownload video!";
+        }
+
+        const videoURL = tiktokData.data.play;
+        const videoURLWatermark = tiktokData.data.wmplay;
+        const infonya_gan = `Judul: ${tiktokData.data.title}\nUpload: ${tiktokData.data.create_time
+            }\n\nSTATUS:\n=====================\nLike = ${tiktokData.data.digg_count
+            }\nKomen = ${tiktokData.data.comment_count}\nShare = ${tiktokData.data.share_count
+            }\nViews = ${tiktokData.data.play_count}\nSimpan = ${tiktokData.data.download_count
+            }\n=====================\n\nUploader: ${tiktokData.data.author.nickname || "Tidak ada informasi penulis"
+            }\n(${tiktokData.data.author.unique_id} - https://www.tiktok.com/@${tiktokData.data.author.unique_id
+            } )\nSound: ${tiktokData.data.music
+            }\n`;
+
+        if (videoURL || videoURLWatermark) {
+            await conn.sendFile( m.chat, videoURL, "tiktok.mp4", `Ini kak videonya\n\n${infonya_gan}`, m, );
+            setTimeout(async () => {
+                //await conn.sendFile( m.chat, videoURLWatermark, "tiktokwm.mp4", `*Ini Versi Watermark*\n\n${infonya_gan}`, m, );
+                //await conn.sendFile( m.chat, `${tiktokData.data.music}`, "lagutt.mp3", "ini lagunya", m, );
+                //conn.reply( m.chat, "•⩊• Ini kak Videonya ૮₍ ˶ᵔ ᵕ ᵔ˶ ₎ა\nDitonton yah ₍^ >ヮ<^₎", m, );
+            }, 1500);
+        } else {
+            throw "Tidak ada tautan video yang tersedia.";
+        }
+    } catch (error1) {
+        conn.reply(m.chat, `Error: ${error1}`, m);
+    }
+};
+
+handler.help = ['tiktok'].map((v) => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^t(t|iktok(d(own(load(er)?)?|l))?|td(own(load(er)?)?|l))$/i
+
+handler.disable = false
+handler.register = true
+handler.limit = true
+
+export default handler
+
+async function tiktokdl(url) {
+    let tikwm = `https://www.tikwm.com/api/?url=${url}?hd=1`
+    let response = await (await fetch(tikwm)).json()
+    return response
+}
+
+async function convertVideoToMp3(videoUrl, outputFileName) {
+    return new Promise((resolve, reject) => {
+        ffmpeg(videoUrl)
+            .toFormat("mp3")
+            .on("end", () => resolve())
+            .on("error", (err) => reject(err))
+            .save(outputFileName);
+    });
+            }
