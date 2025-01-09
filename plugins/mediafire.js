5ec10a459ca39ae20332fc7c4862abac70f80b9e
mrdinesh595 <mrdineshofc@gmail.com>
2025-01-09 21:37:16 +0530

    Create mediafire.js

---
+++ plugins/mediafire.js
@@ -0,0 +1,54 @@
+import axios from 'axios'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.mediafire.com/file/in5j3u2zwoq1x33/BLUR_BLUR_ASIK.zip/file`;
+    
+    let mediafireApi = `https://api.ryzendesu.vip/api/downloader/mediafire?url=${encodeURIComponent(args[0])}`;
+
+    m.reply(wait)
+
+    try {
+        let res = await axios.get(mediafireApi);
+        let data = res.data;
+
+        if (!data || !data.download) throw 'Failed to fetch download link. Please try again later.';
+
+        let { filename, size, mimetype: ext, created, download, cookie } = data;
+
+        let caption = `
+*💌 Name:* ${filename}
+*📊 Size:* ${size}
+*🗂️ Extension:* ${ext}
+*📨 Uploaded:* ${new Date(created).toLocaleString()}
+`.trim();
+
+        let fileRes = await axios.get(download, {
+            headers: {
+                Cookie: cookie
+            },
+            responseType: 'arraybuffer'
+        });
+
+        m.reply(caption);
+        await conn.sendFile(
+            m.chat, 
+            Buffer.from(fileRes.data), 
+            filename, 
+            '', 
+            m, 
+            null, 
+            { mimetype: ext, asDocument: true }
+        );
+    } catch (e) {
+        throw 'An error occurred: ' + e;
+    }
+};
+
+handler.help = ['mediafire'].map(v => v + ' <url>');
+handler.tags = ['downloader'];
+handler.command = /^(mediafire|mf)$/i;
+
+handler.limit = true
+handler.register = true
+
+export default handler
